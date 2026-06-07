5b41834ea0cdcb2203f709c43a863a4c39374254
Jgrey247 <1jtgreaves@gmail.com>
2026-06-07 15:46:33 -0400

    D: modified the main file and the constants and added the player file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -9,6 +10,10 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(
+        x = SCREEN_WIDTH /2,
+        y= SCREEN_HEIGHT /2
+    )
     pygame.display.set_caption("Asteroids")
     while True:
         log_state()
@@ -16,8 +21,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        #draw the player
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+        
         
 
 
